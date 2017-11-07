09c97f401233a80d0901850fe99ea1abbca16407
michjk <michj@gmail.com>
2017-11-07 16:35:08 +0800

    model selection run once again

--- run_model_selection_stack.py
+++ run_model_selection_stack.py
@@ -84,46 +84,7 @@ train_dmat = xgb.DMatrix(x_train_2, y_train_2)
 
 params = initializeDefaultParamsGPU()
 
-gridsearch_params = [
-    (max_depth, min_child_weight)
-    for max_depth in range(2,10)
-    for min_child_weight in range(2,10)
-]
-
 t = time.time()
-
-min_mlogloss = float("Inf")
-best_params = None
-#search for max depth and min_child_weight
-for max_depth, min_child_weight in gridsearch_params:
-    print("CV with max_depth={}, min_child_weight={}".format(max_depth, min_child_weight))
-
-    params['max_depth'] = max_depth
-    params['min_child_weight'] = min_child_weight
-
-    cv_result = xgb.cv(
-        params,
-        train_dmat,
-        num_boost_round=num_boost_round_default,
-        seed=seed,
-        nfold=nfold,
-        metrics=metrics,
-        early_stopping_rounds=20
-    )
-
-    mean_mlogloss = cv_result['test-mlogloss-mean'].min()
-    boost_rounds = cv_result['test-mlogloss-mean'].argmin()
-    print("\tMAE {} for {} rounds".format(mean_mlogloss, boost_rounds))
-    if mean_mlogloss < min_mlogloss:
-        min_mlogloss = mean_mlogloss
-        best_params = (max_depth,min_child_weight)
-
-print("Best params: {}, mlogloss: {}".format(best_params, min_mlogloss))
-max_depth = best_params[0]
-min_child_weight = best_params[1]
-
-params['max_depth'] = max_depth
-params['min_child_weight'] = min_child_weight
 
 gridsearch_params = [
     (subsample, colsample)
@@ -164,6 +125,45 @@ colsample = best_params[1]
 params['subsample'] = subsample
 params['colsample_bytree'] = colsample
 
+gridsearch_params = [
+    (max_depth, min_child_weight)
+    for max_depth in range(2,10)
+    for min_child_weight in range(2,10)
+]
+
+min_mlogloss = float("Inf")
+best_params = None
+#search for max depth and min_child_weight
+for max_depth, min_child_weight in gridsearch_params:
+    print("CV with max_depth={}, min_child_weight={}".format(max_depth, min_child_weight))
+
+    params['max_depth'] = max_depth
+    params['min_child_weight'] = min_child_weight
+
+    cv_result = xgb.cv(
+        params,
+        train_dmat,
+        num_boost_round=num_boost_round_default,
+        seed=seed,
+        nfold=nfold,
+        metrics=metrics,
+        early_stopping_rounds=20
+    )
+
+    mean_mlogloss = cv_result['test-mlogloss-mean'].min()
+    boost_rounds = cv_result['test-mlogloss-mean'].argmin()
+    print("\tMAE {} for {} rounds".format(mean_mlogloss, boost_rounds))
+    if mean_mlogloss < min_mlogloss:
+        min_mlogloss = mean_mlogloss
+        best_params = (max_depth,min_child_weight)
+
+print("Best params: {}, mlogloss: {}".format(best_params, min_mlogloss))
+max_depth = best_params[0]
+min_child_weight = best_params[1]
+
+params['max_depth'] = max_depth
+params['min_child_weight'] = min_child_weight
+
 #search eta
 list_eta = [0.0001, 0.001, 0.01, 0.1, 0.2, 0.3]
 min_mlogloss= float("Inf")
@@ -203,13 +203,7 @@ best_params = None
 
 #search n estimator
 for n in n_estimators:
-    print("CV with eta={}".format(n))
-
-    params = initializeDefaultParams()
-
-    params['max_depth'] = max_depth
-    params['min_child_weight'] = min_child_weight
-    params['eta'] = eta
+    print("n with eta={}".format(n))
 
     cv_results = xgb.cv(
         params,
