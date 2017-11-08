235b891fcdf82e02a88edc155e5c84cdbf3df70b
Shen Juin Lee <slee051@e.ntu.edu.sg>
2017-11-08 21:29:19 +0800

    updated data_constants

--- data_manager/data_constants.py
+++ data_manager/data_constants.py
@@ -16,6 +16,13 @@ signup_app_name = 'signup_app'
 first_device_type_name = 'first_device_type'
 first_browser_name = 'first_browser'
 age_name = 'age'
+action_name = 'action'
+action_type_name = 'action_type'
+action_detail_name = 'action_detail'
+device_type_name = 'device_type'
+secs_elapsed_name = 'secs_elapsed'
 
 #nominal attribute
-nominal_column_list = [gender_name, signup_method_name, signup_flow_name, language_name, affiliate_channel_name, affiliate_provider_name, first_affiliate_tracked_name, signup_app_name, first_device_type_name, first_browser_name]
+nominal_train_column_list = [gender_name, signup_method_name, signup_flow_name, language_name, affiliate_channel_name, affiliate_provider_name, first_affiliate_tracked_name, signup_app_name, first_device_type_name, first_browser_name]
+nominal_session_column_list = [action_name, action_type_name, action_detail_name, device_type_name]
+nominal_train_session_column_list = nominal_train_column_list + nominal_session_column_list
